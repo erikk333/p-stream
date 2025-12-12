7cbc45db993c6d145ba1f353e525045926f08503
Bert <147526653+erikk333@users.noreply.github.com>
2025-12-12 17:40:23 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjkxNjIzYWU2MDYzMGM0MzIxZjljYmYwNjk2NTM3YyIsIm5iZiI6MTc2NTU1NjYyNS41NDEsInN1YiI6IjY5M2M0MTkxYWQ3YmJmZThhOGZjNDkxNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PLpwHA3F_KUzms0SJUBxGSjZ703QTf6EY7xsJeaRw5k",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
