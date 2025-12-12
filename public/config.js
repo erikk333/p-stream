48645ae5ce0d155268153d6cd388016dc6a17c94
Bert <147526653+erikk333@users.noreply.github.com>
2025-12-12 17:41:12 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "simple-proxy1234.erik-m-smouter.workers.dev",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjkxNjIzYWU2MDYzMGM0MzIxZjljYmYwNjk2NTM3YyIsIm5iZiI6MTc2NTU1NjYyNS41NDEsInN1YiI6IjY5M2M0MTkxYWQ3YmJmZThhOGZjNDkxNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PLpwHA3F_KUzms0SJUBxGSjZ703QTf6EY7xsJeaRw5k",
